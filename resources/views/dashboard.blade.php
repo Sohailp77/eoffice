5a64cfa078a5e4a0902528fbbfe5b920c4e6b590
Sohailp77 <sohailp012@gmail.com>
2026-02-07 11:47:00 +0530

    second commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,7 +16,7 @@
                 </a>
             @endif
         </div>
-    </div>
+    </div> 
 
     <!-- Stats Grid -->
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
